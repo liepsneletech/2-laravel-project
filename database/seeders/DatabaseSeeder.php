dc1165186ac49cd7b74496487ea7a701c3332936
liepsnele <liepsnele.tech@gmail.com>
2022-10-09 17:44:34 +0300

    Added listing factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,25 +17,27 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
 
-        Listing::create([
-            "title" => "Laravel Senior Developer",
-            "tags" => "laravel, javacsript",
-            "company" => "Acme Corp",
-            "location" => "Boston, MA",
-            "email" => "email1@email.com",
-            "website" => "https://www.acme.com",
-            "description" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis esse eius, nulla sit, aliquid, accusantium iste odio magni voluptatum nisi dolorem ratione harum veniam magnam corporis obcaecati ab quam suscipit."
-        ]);
+        Listing::factory(6)->create();
 
-        Listing::create([
-            "title" => "2Laravel Senior Developer",
-            "tags" => "2laravel, javacsript",
-            "company" => "2Acme Corp",
-            "location" => "2Boston, MA",
-            "email" => "2email1@email.com",
-            "website" => "2https://www.acme.com",
-            "description" => "2Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis esse eius, nulla sit, aliquid, accusantium iste odio magni voluptatum nisi dolorem ratione harum veniam magnam corporis obcaecati ab quam suscipit."
-        ]);
+        // Listing::create([
+        //     "title" => "Laravel Senior Dev eloper",
+        //     "tags" => "laravel, javacsript",
+        //     "company" => "Acme Corp",
+        //     "location" => "Boston, MA",
+        //     "email" => "email1@email.com",
+        //     "website" => "https://www.acme.com",
+        //     "description" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis esse eius, nulla sit, aliquid, accusantium iste odio magni voluptatum nisi dolorem ratione harum veniam magnam corporis obcaecati ab quam suscipit."
+        // ]);
+
+        // Listing::create([
+        //     "title" => "2Laravel Senior Developer",
+        //     "tags" => "2laravel, javacsript",
+        //     "company" => "2Acme Corp",
+        //     "location" => "2Boston, MA",
+        //     "email" => "2email1@email.com",
+        //     "website" => "2https://www.acme.com",
+        //     "description" => "2Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis esse eius, nulla sit, aliquid, accusantium iste odio magni voluptatum nisi dolorem ratione harum veniam magnam corporis obcaecati ab quam suscipit."
+        // ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
